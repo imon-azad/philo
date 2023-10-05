3e2c8d1bdf2f65500155e38d2a95d3cbf6083ec8
Marian <marian@iMac-de-Marian.local>
2023-10-05 13:08:42 +0200

    aa

--- test/main.c
+++ test/main.c
@@ -6,11 +6,45 @@
 /*   By: esamad-j <esamad-j@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 13:57:24 by esamad-j          #+#    #+#             */
-/*   Updated: 2023/10/03 01:24:33 by esamad-j         ###   ########.fr       */
+/*   Updated: 2023/10/05 04:15:21 by esamad-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void error_msj(int num)
+{
+    if(num == 1)
+        write(1, "Error: invalid number of arguments.\n",36);
+    if(num == 2)
+        write(1, "Error: invalid arguments.\n",26);
+    if(num == 3)
+        write(1, "Error: malloc failed.\n",22);
+    if(num == 4)
+        write(1, "Error: mutex init failed.\n",26);
+    if(num == 5)
+        write(1, "Error: failed to create thread.\n",32);
+    
+}
+
+int init_mutex(t_param *data)
+{
+    int i;
+
+    i = 0;
+    data->fork = 0;
+    data->fork = malloc(sizeof(pthread_mutex_t) * data->n_philo);
+    if(!data->fork)
+        return(error_msj(3), -1);
+    while(i < data->n_philo)
+    {  
+        if(pthread_mutex_init(&data->fork[i], NULL) == -1)
+            return(error_msj(5), -1);
+        i++;
+    }
+    return(0);
+}
+
 
 int check_param(int ac, char **av, t_param *data)
 {   
@@ -18,12 +52,17 @@ int check_param(int ac, char **av, t_param *data)
     data->t_die = ft_atoi(av[2]);
     data->t_eat = ft_atoi(av[3]);
     data->t_sleep = ft_atoi(av[4]);
+    
     if(ac == 6)
     {
         data->n_time_eat = ft_atoi(av[5]);
         if(data->n_time_eat <= 0)
             return (error_msj(2), EXIT_FAILURE);
     }
+    if(data->n_philo)
+        if(init_mutex(data) == -1)
+            return(EXIT_FAILURE);
+            
     if(data->n_philo <= 0 || data->t_die <= 0 || data->t_eat <= 0 || data->t_sleep <= 0)
         return (error_msj(2), EXIT_FAILURE); 
     return (EXIT_SUCCESS);
@@ -37,12 +76,25 @@ void *mi_funcion(void *arg) {
     printf("Hola desde el hilo. Valor recibido: %d\n", *numero);
     return NULL;
 }
-void error_msj(int num)
+
+void    thread_function(void *work)
 {
-    if(num == 1)
-        write(1, "Error: invalid number of arguments.\n",36);
-    if(num == 2)
-        write(1, "Error: invalid arguments.\n",26);
+
+    
+}
+
+int	init_thread(t_param *data, t_philo *philo)
+{
+    int i;
+    
+    i = 0;
+    while (i < data->n_philo)
+    {
+        philo[i].right_fork = philo[(i + 1) % data->n_philo].left_fork;
+        if (pthread_create(&philo->thread_id, NULL, mi_funcion, &philo[i]) != 0)
+            return(error_msj(5),EXIT_FAILURE);
+    }
+    
     
 }
 
@@ -50,7 +102,8 @@ int main(int argc, char **argv)
 {
     t_param data;
     t_philo *philo;
-    
+    int i;
+    i = 0;
     /* pthread_t hilo;
     int valor = 42; */
     
@@ -59,6 +112,25 @@ int main(int argc, char **argv)
     if(check_param(argc, argv, &data) == EXIT_FAILURE)
         return(EXIT_FAILURE);       
     philo = malloc(sizeof(t_philo) * data.n_philo);
+    while(i < data.n_philo)
+    {
+        philo[i].id = i;
+        philo[i].dead = 0;
+        philo[i].meal = 0;
+        philo[i].left_fork = data.fork[i];        
+        i++;
+    }
+    
+    if (init_thread(&data, philo))
+		return (EXIT_FAILURE);
+
+    
+    i = 0;
+    while (i < data.n_philo)
+    {
+        printf("id: %i \n", philo[i].id);
+        i++;
+    }
     
     
   /*   if (pthread_create(&hilo, NULL, mi_funcion, &valor) != 0) {
--- test/philo.h
+++ test/philo.h
@@ -6,7 +6,7 @@
 /*   By: esamad-j <esamad-j@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 14:04:53 by esamad-j          #+#    #+#             */
-/*   Updated: 2023/09/25 13:39:42 by esamad-j         ###   ########.fr       */
+/*   Updated: 2023/10/05 04:12:44 by esamad-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,21 @@ typedef struct s_param
     int t_eat;
     int t_sleep;
     int n_time_eat;
-    int n_forks;
+    pthread_mutex_t *fork;
+
+    
+    
     
 }   t_param;
 
 typedef struct s_philo
 {
     int id;
+    int dead;
+    int meal;
+    pthread_t		thread_id;
+    pthread_mutex_t right_fork;
+    pthread_mutex_t left_fork;
     
 }   t_philo;
 
